2f10a0a4ebc0fec51a630ece6ac8e2bb0d80e55e
Uzma Khan <u_kam_khan@hotmail.com>
2017-12-09 21:03:29 -0500

    Boilerplate code from spring initializer for mongo project. more to come

---
+++ trymongo/src/main/java/com/mongo/trial/trymongo/TrymongoApplication.java
@@ -0,0 +1,12 @@
+package com.mongo.trial.trymongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrymongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrymongoApplication.class, args);
+	}
+}
---
+++ trymongo/src/test/java/com/mongo/trial/trymongo/TrymongoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mongo.trial.trymongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrymongoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
